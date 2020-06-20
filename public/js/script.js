6df2462d666295b881f4b1c633f47b7333cf915a
Osagie Erhabor <erhaborosagie@gmail.com>
2020-06-20 11:08:16 +0100

    Added ability to append sent and received message

--- public/js/script.js
+++ public/js/script.js
@@ -2,9 +2,10 @@ const socket = io('http://localhost:3000/');
 
 const send_box = document.getElementById("send-box");
 let input_box = document.getElementById("message");
+let message_box = document.getElementById("message-box");
 
 socket.on('chat-message', data =>{
-    console.log(data);
+    appendSentMessage(data, "received")
 });
 
 send_box.addEventListener("submit", e => {
@@ -12,4 +13,27 @@ send_box.addEventListener("submit", e => {
     let message = input_box.value;
     socket.emit("sent-message", message);
     input_box.value = '';
+    appendSentMessage(message, "sent")
 });
+
+function appendSentMessage(message, transit) {
+    const messageElement = document.createElement('div');
+    const innerMessage = document.createElement('div');
+    const iconDiv = document.createElement('div');
+    innerMessage.classList.add("message");
+    iconDiv.classList.add("icon");
+    messageElement.classList.add(transit);
+    innerMessage.innerText = message;
+
+    if (transit === "sent"){
+        iconDiv.innerHTML = `<i class="far fa-user"></i>`;
+        messageElement.append(innerMessage);
+        messageElement.append(iconDiv);
+    }else {
+        iconDiv.innerHTML = `<img src="static/images/covidbot.png" alt="covidbot" style="max-width: 30px">`;
+        messageElement.append(iconDiv);
+        messageElement.append(innerMessage);
+    }
+
+    message_box.append(messageElement);
+}
